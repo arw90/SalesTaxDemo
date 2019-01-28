56899a9cade6f95294d3406bfcc101018fe27fd4
Alex Wolf <alexrudiwolf@gmail.com>
2019-01-28 13:14:16 -0500

    version fix

--- src/SalesTaxStore/Startup.cs
+++ src/SalesTaxStore/Startup.cs
@@ -53,7 +53,7 @@ namespace SalesTaxStore
             services.AddTransient<ITaxService<SalesTaxService>, SalesTaxService>();
             services.AddTransient<ITaxService<ImportTaxService>, ImportTaxService>();
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
